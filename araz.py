3ceb7d787be7133a50ef3e40a212de4d2b23c60c
darkdragon630 <71604929+darkdragon630@users.noreply.github.com>
2024-12-21 21:24:46 +0700

    Add files via upload

---
+++ araz.py
@@ -0,0 +1,15 @@
+"""
+user = 6
+for i in range(user):
+    nama = input("masukan nama : ")
+    print = (f"data user {i+1}: {user}")
+
+"""
+
+buah = ["buah", "roti", "sate"]
+for makanan in buah :
+    print(f"aku suka makan", makanan)
+
+mylist = ['apple', 'banana', 'cherry']
+mylist[1:2] = ['kiwi', 'mango']
+print(mylist[2])
